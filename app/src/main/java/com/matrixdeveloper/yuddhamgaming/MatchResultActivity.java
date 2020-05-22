95d5de56d3c82246530f8176528d0ae8a28afaf1
shrawan03 <shrawank039@gmail.com>
2020-05-23 01:30:15 +0530

    Profile, login, register, withdraw, wallet

---
+++ app/src/main/java/com/matrixdeveloper/yuddhamgaming/MatchResultActivity.java
@@ -0,0 +1,14 @@
+package com.matrixdeveloper.yuddhamgaming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_result);
+    }
+}
